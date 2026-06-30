c2a03f58515b2f5b854fc0b78bd8976802ca6200
pratiksha-waghmare0606 <pratiksha.waghmare@smartkargo.com>
2026-06-30 15:51:40 +0530

    added speech to text functionality

---
+++ app/components/hook/useSpeechRecognition.ts
@@ -0,0 +1,62 @@
+import { useRef, useState } from "react";
+
+
+declare global {
+  interface Window {
+    SpeechRecognition: any;
+    webkitSpeechRecognition: any;
+  }
+}
+
+export function useSpeechRecognition() {
+  const [isListening, setIsListening] = useState(false);
+
+  const recognitionRef = useRef<any>(null);
+
+  const startListening = (onResult: (text: string) => void) => {
+    const SpeechRecognition =
+      window.SpeechRecognition || window.webkitSpeechRecognition;
+
+    if (!SpeechRecognition) {
+      alert("Speech Recognition is not supported in this browser.");
+      return;
+    }
+
+    const recognition = new SpeechRecognition();
+
+    recognition.lang = "en-US";
+    recognition.interimResults = true;
+    recognition.continuous = true;
+
+    recognition.onstart = () => setIsListening(true);
+
+    recognition.onend = () => setIsListening(false);
+
+    recognition.onresult = (event: SpeechRecognitionEvent) => {
+    let transcript = "";
+
+    for (let i = event.resultIndex; i < event.results.length; i++) {
+        transcript += event.results[i][0].transcript;
+    }
+
+    onResult(transcript.trim());
+    };
+
+    recognition.start();
+
+    recognitionRef.current = recognition;
+  };
+
+  const stopListening = () => {
+        if (recognitionRef.current) {
+            recognitionRef.current.stop();
+            setIsListening(false);
+        }
+    };
+
+  return {
+    startListening,
+    stopListening,
+    isListening,
+  };
+}
